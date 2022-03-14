151e477fe731671b268a2c0d14bc7135bc20eab4
Kathy Huang <khuang2@stanford.edu>
2022-03-13 21:57:10 -0700

    change to 4 wire interface

--- code/icsp.h
+++ code/icsp.h
@@ -1,5 +1,12 @@
 #include "rpi.h"
 
+#define HI_TIME 50
+#define LO_TIME 50
+#define TDI 5 // pin 16 on PIC
+#define TCK 6 // pin 17 on PIC
+#define TDO 13 // pin 18 on PIC
+#define TMS 19 // pin 22 on PIC
+
 void enter_programming_mode(void);
 
 void read_device_status(void);
