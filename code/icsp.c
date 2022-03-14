81d1306b269c7db4537a5d78dbea3fb7c1bd6c45
Ben Choi <choiben314@gmail.com>
2022-03-13 21:29:31 -0700

    Push initial ICSP header file

--- code/icsp.c
+++ code/icsp.c
@@ -1,1 +1,24 @@
 #include "icsp.h"
+
+
+
+// Enter enhanced ICSP
+
+// MCLR pin driven high, then low
+// 32-bit key sequence is clocked into PGDx
+// MCLR driven high with specific period of time and held
+
+
+
+
+// Read device status
+
+// SetMode (6'b011111) to force TAP controller into Run Test/Idle state
+
+// SendCommand(MTAP_SW_MTAP)
+// SendCommand(MTAP_COMMAND)
+// statusVal = XferData(MCHP_STATUS)
+// If CFGRDY (statusVal<3>) is 0 and FCBUSY (statusVal<2>) is 1
+    // delay 10ms
+    // read Xfer data again
+// Done.
--- code/icsp.h
+++ code/icsp.h
@@ -1,1 +1,11 @@
 #include "rpi.h"
+
+void enter_programming_mode(void);
+
+void read_device_status(void);
+
+void set_mode(uint8_t mode);
+
+void send_command(uint8_t cmd);
+
+void xfer_data(uint32_t status);
