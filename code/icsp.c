8d47e2c5afc5a229042f3691d98fdf8b88d1dae2
Ben Choi <choiben314@gmail.com>
2022-03-13 21:59:55 -0700

    Configure IO.

--- code/icsp.c
+++ code/icsp.c
@@ -1,14 +1,41 @@
 #include "icsp.h"
 
 
+void configure_io(void) {
+    gpio_set_output(PIN_MCLR);
+    gpio_set_output(PIN_TCK);
+    gpio_set_output(PIN_TDI);
+    gpio_set_input(PIN_TDO);
+    gpio_set_off(PIN_MCLR);
+    gpio_set_off(PIN_TCK);
+    gpio_set_off(PIN_TDI);
+}
 
 // Enter enhanced ICSP
+
+void enter_programming_mode(void) {
+    unimplemented();
+    // gpio_set_on(PIN_MCLR);
+    // delay_us(10);
+    // gpio_set_off(PIN_MCLR);
+    // delay_us(300);
+
+    // // send KEY_SEQ
+    
+    // gpio_set_on(PIN_MCLR);
+    // delay_us(1);
+}
 
 // MCLR pin driven high, then low
 // 32-bit key sequence is clocked into PGDx
 // MCLR driven high with specific period of time and held
 
 
+// Clock goes high
+// Set PGD
+// Delay by some amount
+// Clock goes low
+// 
 
 
 // Read device status
--- code/icsp.h
+++ code/icsp.h
@@ -1,5 +1,10 @@
 #include "rpi.h"
 
+#define KEY_SEQ 0b01001101010000110100100001010000
+
+void configure_io(void);
+
+// Only needed for two-wire mode
 void enter_programming_mode(void);
 
 void read_device_status(void);
